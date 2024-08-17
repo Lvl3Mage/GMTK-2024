c9312b1a6afdda33134fc9843634f5b0e986b4b8
Jeruko <jeruizko10@gmail.com>
2024-08-17 18:04:30 +0200

    PlantRenderer in progress

--- Assets/Joan Scripts/PlantRenderer.cs
+++ Assets/Joan Scripts/PlantRenderer.cs
@@ -1,25 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlantRenderer : MonoBehaviour
 {
-    void Add(Vector2Int[] cells)
+    public Dictionary<Vector2Int, PlantCellRenderer> renderers = new();
+    [SerializeField] PlantCellRenderer cellRendererPrefab; 
+
+    public void Add(Vector2Int[] cells)
     {
-        for (int i = 0; i < cells.Length; i++)
+        HashSet<Vector2Int> updatedCells = new(cells);
+
+        foreach (Vector2Int cell in cells)
         {
-            
+            Vector2Int[] neighbours = CellUtils.GetCellNeighbours(cell);
+
+            updatedCells.AddRange<Vector2Int>(neighbours);
+            updatedCells.Add(cell);
+            renderers.Add(cell, Instantiate(cellRendererPrefab, (Vector2)cell, Quaternion.identity));
+        }
+
+        UpdateSprites(updatedCells.ToList<Vector2Int>());
+    }
+
+    public void UpdateSprites(List<Vector2Int> cells)
+    {
+        foreach (Vector2Int cell in cells)
+        {
+            int spriteIndex = GetTileIndex(cell);
+            //cell.asignTheFokinSprite(spriteIndex);
         }
     }
 
-    void Start()
+    public int GetTileIndex(Vector2Int cell)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        //if water return 0
+        //if plant return 1...
+        return 0;
     }
 }
