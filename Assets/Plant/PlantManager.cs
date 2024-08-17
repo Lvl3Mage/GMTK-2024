fa68ac0648887b8beb610464a0329c4f66d52542
Karl <lvlthreemage@gmail.com>
2024-08-17 23:52:27 +0200

    Man I'm tired of this

--- Assets/Plant/PlantManager.cs
+++ Assets/Plant/PlantManager.cs
@@ -3,9 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlantManager : MonoBehaviour
-{
-    
-    private List<Plant> plants = new List<Plant>();
+{ 
+    List<Plant> plants = new();
     [SerializeField] Plant plantPrefab;
 
 
@@ -14,20 +13,18 @@ public class PlantManager : MonoBehaviour
     /// </summary>
     /// <param name="positions">the coordinates where it will be created</param>
     /// <param name="rootPosition">the coordinates of the first Plant</param>
-    public void CreatePlant(Vector2Int[] positions, Vector2Int rootPosition)
+    void CreatePlant(Vector2Int[] positions, Vector2Int rootPosition)
     {
-        Plant newPlant = InstantiatePlantAt(rootPosition);
+        Plant newPlant = Instantiate(plantPrefab, (Vector2)rootPosition, Quaternion.identity);
         newPlant.Create(positions, rootPosition);
-
-        //foreach (var position in positions)
-        //{
-        //    newPlant.AddPlantPosition(position);
-        //}
         plants.Add(newPlant);
         newPlant.OnDestroyed += RemovePlant;
     }
+    
+    
+    
 
-    private void RemovePlant(Plant plant)
+    void RemovePlant(Plant plant)
     {
         plants.Remove(plant);
         Debug.Log($"Plant removed from root position {plant.transform.position}");
@@ -40,19 +37,5 @@ public class PlantManager : MonoBehaviour
             plant.Grow();
         }
     }
-
-    private Plant InstantiatePlantAt(Vector2Int position)
-    {
-        Plant plant = Instantiate(plantPrefab);
-
-        plant.transform.position = new Vector2(position.x, position.y);
-        return plant;
-    }
-static float easeInBack(float x) {
-    const float c1 = 1.70158f;
-    const float c3 = c1 + 1;
-
-    return 1 + c3 * Mathf.Pow(x - 1, 3) + c1 * Mathf.Pow(x - 1, 2);
-}
     
 }
