10ce9e3b5b20752f34ae8290f454a5ecfba2c245
Karl <lvlthreemage@gmail.com>
2024-08-18 03:42:11 +0200

    Basic Cell rendering working

--- Assets/Plant/PlantCellRenderer.cs
+++ Assets/Plant/PlantCellRenderer.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
@@ -7,26 +8,42 @@ using UnityEngine.Serialization;
 
 public class PlantCellRenderer : MonoBehaviour
 {
-    [SerializeField] Renderer tileRenderer;
+    [SerializeField] Renderer[] tileRenderers;
     [SerializeField] Animator animator;
-    public int GetSubTileIndex(bool[] edges)
+    bool[] currentEdges = new bool[9];
+    int GetSubTileIndex(bool[] edges)
     {
         int index = 0;
-        foreach (bool edge in edges){
-            index += edge ? 1 : 0;
+        for (int i = edges.Length-1; i >= 0; i--){
             index <<= 1;
+            index += edges[i] ? 1 : 0;
         }
 
         return index;
     }
 
-    int[] GetTileIndecies(bool[] edges)
+    int[] GetTileIndices(bool[] edges)
     {
-        int [] indices = new int[]{
-            GetSubTileIndex(new bool[]{edges[0],edges[1],edges[3],edges[4]}),
-            GetSubTileIndex(new bool[]{edges[1],edges[2],edges[4],edges[5]}),
-            GetSubTileIndex(new bool[]{edges[3],edges[4],edges[6],edges[7]}),
-            GetSubTileIndex(new bool[]{edges[4],edges[5],edges[7],edges[8]}),
+        bool[][] subTiles = 
+        {
+            new[]{edges[0],edges[1],edges[3],edges[4]},
+            new[]{edges[1],edges[2],edges[4],edges[5]},
+            new[]{edges[3],edges[4],edges[6],edges[7]},
+            new[]{edges[4],edges[5],edges[7],edges[8]}
+        };
+        for (int i = 0; i < 4; i++){
+            bool filled = true;
+            for (int j = 0; j < 4; j++){
+                filled &= subTiles[i][j];
+            }
+        
+            subTiles[i][i] = filled;
+        }
+        int[] indices = new int[]{
+            GetSubTileIndex(subTiles[0]),
+            GetSubTileIndex(subTiles[1]),
+            GetSubTileIndex(subTiles[2]),
+            GetSubTileIndex(subTiles[3]),
         };
 
         return indices;
@@ -34,9 +51,11 @@ public class PlantCellRenderer : MonoBehaviour
 
     public void SetTileData(bool[] edges)
     {
-        int[] indices = GetTileIndecies(edges);
-        Vector4 indexData = new Vector4(indices[0], indices[1], indices[2], indices[3]);
-        tileRenderer.material.SetVector("_TileIndex", indexData);
+        currentEdges = edges;
+        int[] indices = GetTileIndices(edges);
+        for (int i = 0; i < tileRenderers.Length; i++){
+            tileRenderers[i].material.SetInt("_TileIndex", indices[i]);
+        }
         //Update shaders
     }
 
@@ -45,6 +64,19 @@ public class PlantCellRenderer : MonoBehaviour
         if (animate)
         {
             //animator
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Vector2Int position = new Vector2Int((int)transform.position.x, (int)transform.position.y);
+        Vector2Int[] neighbours = CellUtils.GetCellNeighbours(position, true);
+        
+        for (int i = 0; i < neighbours.Length; i++)
+        {
+            Vector2Int neighbour = neighbours[i];
+            Gizmos.color = currentEdges[i] ? Color.green : Color.red;
+            Gizmos.DrawWireSphere((Vector2)neighbour, 0.1f);
         }
     }
 }
