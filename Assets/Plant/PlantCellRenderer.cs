7dba3233a3bd1357be9458ce5562ce1191d6d714
Jeruko <jeruizko10@gmail.com>
2024-08-18 02:02:14 +0200

    PlantRenderer and PlantCellRenderer Finished

--- Assets/Plant/PlantCellRenderer.cs
+++ Assets/Plant/PlantCellRenderer.cs
@@ -9,18 +9,70 @@ public class PlantCellRenderer : MonoBehaviour
 {
     [SerializeField] Renderer tileRenderer;
     [SerializeField] Animator animator;
-    public int GetSubTileIndex(bool[] edges)
+
+    bool[] edges = new bool[8]; //true = edge is on collision | false = it is not
+    bool alreadySpawned = false; //Makes an spawn animation play if updated for the first time
+
+    /// <summary>
+    /// Modifies the whole cell edges. Each PlantRenderer uses it to define its childs. 
+    /// </summary>
+    /// <param name="neighbouredEdges">An array that reprents the edges that are on collision with other cells</param>
+    public void SetTileEdges(bool[] neighbouredEdges)
     {
+        edges = neighbouredEdges;
+    }
+
+    /// <summary>
+    /// Plays the update or spawn animation. It is called from each PlantRenderer to animate cells when updating them. 
+    /// </summary>
+    public void TriggerAnimation()
+    {
+        if (!alreadySpawned) animator.Play("Spawn");
+        else animator.Play("Update");
+    }
+
+    /// <summary>
+    /// Updates the rendered sprite so that it matches stored edge data. (Called by Animator on precise frames). 
+    /// </summary>
+    public void UpdateSprite()
+    {
+        int[] indices = GetTileIndices();
+        Vector4 indexData = new Vector4(indices[0], indices[1], indices[2], indices[3]);
+        tileRenderer.material.SetVector("_TileIndex", indexData);
+        alreadySpawned = true; //Will no longer play spawn animation, instead it will use Update animation
+    }
+
+    /// <summary>
+    /// Plays a destroying animation and then deletes the instance of the cell renderer. 
+    /// </summary>
+    /// <param name="animate">Whether the instance will play or not its destroy animation before being deleted</param>
+    public void DestroyCell(bool animate = true)
+    {
+        if (animate) animator.Play("Destroy"); //Delete will be called on animation end
+        else Delete(); //Instant deletion
+    }
+
+    /// <summary>
+    /// DO NOT USE. CALL "DestroyCell()" INSTEAD! (This needs to be public for animation purposes). 
+    /// </summary>
+    public void Delete()
+    {
+        Destroy(gameObject);
+    }
+
+    int GetSubTileIndex(bool[] corners)
+    {
+        //Gets the sprite index for a subtile (one of the four small tiles that form a normal one). 
         int index = 0;
-        foreach (bool edge in edges){
-            index += edge ? 1 : 0;
+        foreach (bool corner in corners){
+            index += corner ? 1 : 0;
             index <<= 1;
         }
 
         return index;
     }
 
-    int[] GetTileIndecies(bool[] edges)
+    int[] GetTileIndices() //Returns 4 indices (the sub-sprites)
     {
         int [] indices = new int[]{
             GetSubTileIndex(new bool[]{edges[0],edges[1],edges[3],edges[4]}),
@@ -30,21 +82,5 @@ public class PlantCellRenderer : MonoBehaviour
         };
 
         return indices;
-    }
-
-    public void SetTileData(bool[] edges)
-    {
-        int[] indices = GetTileIndecies(edges);
-        Vector4 indexData = new Vector4(indices[0], indices[1], indices[2], indices[3]);
-        tileRenderer.material.SetVector("_TileIndex", indexData);
-        //Update shaders
-    }
-
-    public void Destroy(bool animate = true)
-    {
-        if (animate)
-        {
-            //animator
-        }
     }
 }
--- Assets/Plant/PlantRenderer.cs
+++ Assets/Plant/PlantRenderer.cs
@@ -8,7 +8,7 @@ using UnityEngine.Rendering.Universal;
 
 public class PlantRenderer : MonoBehaviour
 {
-    Dictionary<Vector2Int, PlantCellRenderer> childRenderers = new(); //All 'child' bush cells the whole plant has
+    Dictionary<Vector2Int, PlantCellRenderer> childRenderers = new(); //All 'child' cells the whole plant currently has
     [SerializeField] PlantCellRenderer cellRendererPrefab;
 
     public void AddCell(Vector2Int cell)
@@ -21,58 +21,58 @@ public class PlantRenderer : MonoBehaviour
     /// <param name="cells">Each of the cell coordinates that the plant has. </param>
     public void AddCells(Vector2Int[] cells)
     {
+        //updatedCells will store the cells where bushes will be added + cells that will change due to those bushes
         HashSet<Vector2Int> updatedCells = new(cells);
 
         foreach (Vector2Int cell in cells)
         {
             Vector2Int[] neighbours = CellUtils.GetCellNeighbours(cell);
 
-            updatedCells.AddRange<Vector2Int>(neighbours);
-            updatedCells.Add(cell);
+            updatedCells.AddRange<Vector2Int>(neighbours); //Add neighbours
+            updatedCells.Add(cell); //Add itself
             childRenderers.Add(cell, Instantiate(cellRendererPrefab, (Vector2)cell, Quaternion.identity));
         }
 
-        UpdateSprites(updatedCells.ToList<Vector2Int>());
+        UpdateSprites(updatedCells.ToList<Vector2Int>()); //Modify all cell tiles that need to be changed
     }
 
-    /// <summary>
-    /// Changes the sprites of a given coordinate list, according to the state of WorldGrid. 
-    /// </summary>
-    /// <param name="cells">Each of the cell coordinates that will be updated. </param>
-    public void UpdateSprites(List<Vector2Int> cells)
+    void UpdateSprites(List<Vector2Int> cells)
     {
+        // Changes the sprites of a given coordinate (cell) list, according to the state of WorldGrid. 
         foreach (Vector2Int cell in cells)
         {
             //Update each cell
             PlantCellRenderer cellRenderer;
 
-            if (childRenderers.ContainsKey(cell))
+            if (childRenderers.ContainsKey(cell)) //Guaranteed to happen
             {
                 cellRenderer = childRenderers[cell];
-                
                 Vector2Int[] tileData = CellUtils.GetCellNeighbours(cell, true);
-                bool[] neighbouredEdges = new bool[tileData.Length];
+                bool[] neighbouredEdges = new bool[tileData.Length]; //tileData.Length is always 9
                 
                 for (int i = 0; i < tileData.Length; i++)
                 {
-                    neighbouredEdges[i] = WorldGrid.instance.GetPlantAt(tileData[i]) != null;
+                    //WorldGrid.instance.GetPlantAt(tileData[i]) != null; (use to make cells combine even if they come from different plants)
+                    neighbouredEdges[i] = childRenderers.ContainsKey(tileData[i]); //cells only take into account other cells from their plant
                 }
 
-                cellRenderer.SetTileData(neighbouredEdges);
+                cellRenderer.SetTileEdges(neighbouredEdges);
+                cellRenderer.TriggerAnimation();
             }
             else Debug.LogWarning("Trying to render an unexisting cell. ");
         }
     }
     
     /// <summary>
-    /// Iterates over the renderers of all childs/cells and calls Destroy() on each of them. 
+    /// Iterates over the renderers of all childs/cells and calls Destroy() on each of them. Then, it destroys itself. 
     /// </summary>
     /// <param name="animate">Determines if an animation will be played when destroyed. </param>
-    public void DestroyCells(bool animate = true)
+    public void DestroyAll(bool animate = true)
     {
         foreach (KeyValuePair<Vector2Int, PlantCellRenderer> pair in childRenderers)
         {
-            pair.Value.Destroy(animate); //Each child manages its own animation
+            pair.Value.DestroyCell(animate); //Each child manages its own animation
         }
+        Destroy(gameObject);
     }
 }
